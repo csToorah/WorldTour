33dc814fcdde01bdeb701281885609e424ad1da2
Cesar Amezola <109708622+csToorah@users.noreply.github.com>
2024-08-18 10:49:17 -0700

    update

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const http = require('http')
-const {scrapeData} = require('../client/scripts/index.js')
 
 const PORT = process.env.PORT || 3500
 
 const server = http.createServer(function(req, res){
     async function createWrite(){
         res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end(await scrapeData('https://www.mlb.com'))
+        res.end()
     }
     createWrite()
 })
