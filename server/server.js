285cb89815746628b295ccdb23c95a0e2315f018
Cesar Amezola <cesar.amezola@icloud.com>
2024-08-24 20:48:46 -0700

    scrapeAll() works

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const puppeteer = require('puppeteer')
-const fs = require('fs')
+const fs = require('fs');
+const { Console } = require('console');
 const fsPromises = require('fs').promises
 
 openBrowser().then(values =>{
+    console.log(values)
     fs.writeFile('./client/json/data.json', JSON.stringify(values, null, 2),'utf-8' , (err)=>{
         if(err){console.log(err)}
         console.log('Saved!')
@@ -13,7 +15,7 @@ openBrowser().then(values =>{
 async function openBrowser(){
     let browser = await puppeteer.launch()
     let page = await browser.newPage()
-    await page.goto('https://www.mlb.com/gameday/mariners-vs-pirates/2024/08/18/745464/live/box')
+    await page.goto('https://www.mlb.com/scores')
 
     const mlbScoresObject = {
         home: {
@@ -38,16 +40,34 @@ async function openBrowser(){
         return object;
     }
 
-    return scrape(mlbScoresObject)
+    const scrapeAll = async(selector)=>{
+        let gameContainers = await page.$$(selector)
+        let teams = [];
+
+        for await(const gameContainer of gameContainers){
+            let teamNames = await gameContainer.$$('.fdaoCu');
+            let namesArray = []
+            for await(const name of teamNames){
+                if(namesArray.length < 2) namesArray.push(await name.evaluate(el => el.textContent))
+            }
+            teams.push({home: namesArray[0], visitor: namesArray[1]})
+            namesArray =[]
+        }
+        console.log(teams)
+        return 'Hello'
+    }
+    return scrapeAll('.gmoPjI')
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 
 const server = http.createServer(function(req, res){
     console.log(req.url)
     async function readFile(){
+        if(req.url == '/')
         try{
-            let data = await readFile('./client/json/data.json')
+            let data = await fsPromises.readFile('./client/views/index.html')
+            console.log('i sent the file')
 
             res.writeHead(200, {"Content-Type": "text/html"})
             res.end(data)
